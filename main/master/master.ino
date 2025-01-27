a0b39dcde04b5767d3168e93b6f2ac9986896c24
lilmrmagoo <lilmrmagoo@gmail.com>
2025-01-27 00:16:38 -0400

    core master logic, untested

--- main/master/master.ino
+++ main/master/master.ino
@@ -9,15 +9,20 @@ BluetoothA2DPSink a2dp_sink(out);
 int CurrentCycle = 0;
 int NextCycle = 1;
 int PrevCycle = 2;
-bool playback = true;
-bool lastPlayState = true;
-bool states[] = {0,0,0};
+byte states[] = {0,0,0};
 bool triMode = false;
+uint8_t track_change_flag = 0;
+esp_avrc_playback_stat_t lastPlayState = esp_avrc_playback_stat_t::ESP_AVRC_PLAYBACK_PLAYING;
+esp_avrc_playback_stat_t playbackState = esp_avrc_playback_stat_t::ESP_AVRC_PLAYBACK_PLAYING;
+bool globalPlayback = true;
+bool lastGlobalPlayback = true;
+
 
 void avrc_metadata_callback(uint8_t data1, const uint8_t *data2) {
   Serial.printf("AVRC metadata rsp: attribute id 0x%x, %s\n", data1, data2);
 }
 void avrc_rn_playstatus_callback(esp_avrc_playback_stat_t playback) {
+  playbackState = playback;
   switch (playback) {
     case esp_avrc_playback_stat_t::ESP_AVRC_PLAYBACK_STOPPED:
       Serial.println("Stopped.");
@@ -48,7 +53,7 @@ void avrc_rn_track_change_callback(uint8_t *id) {
     Serial.printf("\tByte %d : 0x%x \n",i,id[i]);
   }
   //An example of how to project the pointer value directly as a uint8_t
-  uint8_t track_change_flag = *id;
+  track_change_flag = *id;
   Serial.printf("\tFlag value: %d\n",track_change_flag);
 }
 void setup() {
@@ -56,6 +61,7 @@ void setup() {
   Serial.begin(115200);
   a2dp_sink.set_avrc_metadata_callback(avrc_metadata_callback);
   a2dp_sink.set_avrc_rn_track_change_callback(avrc_rn_track_change_callback)
+  a2dp_sink.set_avrc_rn_playstatus_callback(avrc_rn_playstatus_callback)
   a2dp_sink.start("BTCycler1");
   while(!a2dp_sink.is_connected()) 
     delay(1000);
@@ -63,26 +69,36 @@ void setup() {
 
 void loop() {
 }
+//state1 nothing, state2 cycle, state3 refersh.
 int readState() {
-  //state1 cycle, state2 refresh, state3 do nothing.
-  if(playback != lastPlayState){toggle(CurrentCycle);timeoutCheck(firstInputPin+CurrentCycle, playback);Serial.println("Paused = "+String(playback)); return 3;} // if paused or play button is triggered toggle the current device
+  
+  if (playbackState == esp_avrc_playback_stat_t::ESP_AVRC_PLAYBACK_PLAYING){
+    states[0] = 1;
+  }
+
+  Wire.requestFrom(1,1)
+  states[1] = Wire.read()
+  if(triMode){
+    Wire.requestFrom(1,1)
+    states[2] = Wire.read()
+  } 
+   // if paused or play button is triggered toggle the current device
+  if(globalPlayback != lastGlobalPlayback){
+    toggle(CurrentCycle);
+    Serial.println("Paused = "+String(globalPlayback));
+    return 1;
+  }
   int playing = 0;
   for(int i = 0; i<3;i++){
-    int read = digitalRead(firstInputPin+i);
-    if(read>=HIGH){
-      playing++;
-      if(states[i]!=read){
-        states[i] = read;
-        Serial.println("device " + String(i) + " is " + String(read));
-      }  
-    }
+    if(states[i] == 0x01){playing++;}
+    Serial.println("device " + String(i) + " is " + String(states[i]));
   }
-  if(cycleFlag && playback){return 1;}
-  if(playing == 0&&playback){
-  	return 1;
-  }
-  else if(playing > 1){
+  if(cycleFlag && globalPlayback){return 1;}
+  if(playing == 0 && !globalPlayback){
   	return 2;
   }
-  else{return 3;}
+  else if(playing > 1){
+  	return 3;
+  }
+  else{return 1;}
 }
